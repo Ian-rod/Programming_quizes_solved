a1256491d8af4622ffc5c4930a0aa0277d72c652
Ian-rod <ianomengo@gmail.com>
2026-04-02 10:09:07 +0300

    Dividing two numbers without division

---
+++ DivisionTwosComplement.java
@@ -0,0 +1,44 @@
+/*
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+Return the quotient after dividing dividend by divisor.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+*/
+
+
+public class DivisionTwosComplement {
+    public int divide(int dividend, int divisor) {
+    //subtract until dividend is <divisor
+    boolean negative=false;
+    if(divisor<0&&dividend>0||dividend<0&&divisor>0)
+    {
+        negative=true;
+    }
+   long dividendLong=Math.abs((long)dividend);
+   long divisorLong=Math.abs((long)divisor);
+    long result=0;
+    while (dividendLong>=divisorLong) {
+        dividendLong-=divisorLong;
+        result++;
+    }
+    result=negative?-result:result;
+    if(result>=Integer.MAX_VALUE)
+    {
+        return Integer.MAX_VALUE;
+    }
+    else if(result<Integer.MIN_VALUE){ 
+        return Integer.MIN_VALUE;
+    }
+   
+    return (int)result;
+}
+    public void main(String[] args) {
+       System.out.println(divide(-2147483648, 4));
+       //System.out.println(divide(7, -3));
+       // System.out.println(Math.abs((long)-2147483648));
+    }
+}
