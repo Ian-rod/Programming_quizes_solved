e1c495e6a34c49a3122d48cd35bfdc738124e2b9
Ian-rod <ianomengo@gmail.com>
2026-06-26 12:46:07 +0300

    Finding a duplicate number solution 1

---
+++ DuplicateNumber.java
@@ -0,0 +1,51 @@
+/*
+Find the Duplicate Number
+Medium
+Topics
+Company Tags
+Hints
+You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
+
+There is exactly one repeated integer in nums, and every other integer appears at most once.
+
+Return the repeated integer.
+
+Example 1:
+
+Input: nums = [1,2,3,2,2]
+
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3,4,4]
+
+Output: 4
+Follow-up: Can you solve the problem without modifying the array nums and using 
+O
+(
+1
+)
+O(1) extra space?
+
+Constraints:
+
+1 <= n <= 10,000
+nums.length == n + 1
+1 <= nums[i] <= n
+
+*/
+
+public class DuplicateNumber {
+     public int findDuplicate(int[] nums) {
+       for(int i=0;i<nums.length;i++)
+       {
+        for (int l = i+1; l < nums.length; l++) {
+            if(nums[i]==nums[l])
+            {
+                return nums[i];
+            }
+        }
+       }
+        return 0;
+    } 
+}
